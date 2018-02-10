30db829e60c60b724011a809448221c5b167a5fb
scarweb <webmaster@sc-aufruhr.de>
2018-02-10 16:04:43 +0100

    Update contact_me.php

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -22,8 +22,8 @@ $message = $_POST['message'];
 
 // Create the email and send the message
 $to = 'Vorstand@sc-aufruhr.de'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
-$email_subject = "Website Contact Form:  $name";
-$email_body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nPhone: $phone\n\nMessage:\n$message";
+$email_subject = "Website-Kontaktformular:  $name";
+$email_body = "Sie haben eine neue Nachricht von Ihrem Website-Kontaktformular erhalten.\n\n"."Hier sind die Details:\n\nName: $name\n\nEmail: $email_address\n\nPhone: $phone\n\nMessage:\n$message";
 $headers = "From: Vorstand@sc-aufruhr.de\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
 $headers .= "Reply-To: $email_address";
 mail($to,$email_subject,$email_body,$headers);
